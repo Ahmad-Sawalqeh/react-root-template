e651bb56ceb85afc57c6785f929c988bb6edb68e
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-08-24 08:59:07 +0300

    creating home section

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/header';
+import Home from './components/home';
 
 function App() {
   return (
+    <>
       <Header />
+      <Home />
+    </>
   );
 }
 
---
+++ src/components/home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home-content d-flex justify-content-center align-items-center p-5' >
+        <div className='position-absolute z-index-1 container text-center text-light'>
+        <h1 className='title'>WELCOME</h1>
+        <h2 className='sub-title mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            laborum minus molestiae.
+        </h2>
+        <button className='btn btn-primary rounded-0 mr-2'>
+            Learn More
+        </button>
+        <button className='btn btn-light text-dark rounded-0'>
+            Contact Us
+        </button>
+        </div>
+    </div>
+  );
+};
+
+export default Home;
