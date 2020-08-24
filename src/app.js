cf577bc6b24a6a5ae64caae2231b75c8addfdee8
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-08-24 10:44:55 +0300

    creating services section

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header';
 import Home from './components/home';
 import About from './components/about';
+import Services from './components/services';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Services />
     </>
   );
 }
---
+++ src/components/services/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import './services.css';
+
+const Services = () => {
+  return (
+    <div id='services'>
+      <div className='container py-2'>
+        <div className='section-header py-5 text-center'>
+          <h3 className='section-title'>
+            <span>Our </span>Services
+          </h3>
+          <h6 className='section-subtitle mx-auto'>
+            Individualized quality care that meets the total needs of the
+            patient Individualized quality care that quality care that
+            Individualized quality care that meets the total.
+          </h6>
+        </div>
+        <div className='section-content'>
+          <div className='row'>
+            <div className='col-lg-4 mb-3'>
+              <div className='service-box d-flex'>
+                <div className='service-icon mr-4'>
+                  <i className='fas fa-briefcase' />
+                </div>
+                <div className='service-body'>
+                  <h5 className='service-title'>Business Planning</h5>
+                  <p className='service-description'>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 mb-3'>
+              <div className='service-box d-flex'>
+                <div className='service-icon mr-4'>
+                  <i className='fas fa-chart-bar' />
+                </div>
+                <div className='service-body'>
+                  <h5 className='service-title'>Product Marketing</h5>
+                  <p className='service-description'>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 mb-3'>
+              <div className='service-box d-flex'>
+                <div className='service-icon mr-4'>
+                  <i className='fas fa-fist-raised' />
+                </div>
+                <div className='service-body'>
+                  <h5 className='service-title'>Risk Management</h5>
+                  <p className='service-description'>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 mb-3'>
+              <div className='service-box d-flex'>
+                <div className='service-icon mr-4'>
+                  <i className='far fa-paper-plane' />
+                </div>
+                <div className='service-body'>
+                  <h5 className='service-title'>Market Research</h5>
+                  <p className='service-description'>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 mb-3'>
+              <div className='service-box d-flex'>
+                <div className='service-icon mr-4'>
+                  <i className='fas fa-gem' />
+                </div>
+                <div className='service-body'>
+                  <h5 className='service-title'>Business Financing</h5>
+                  <p className='service-description'>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 mb-3'>
+              <div className='service-box d-flex'>
+                <div className='service-icon mr-4'>
+                  <i className='far fa-life-ring' />
+                </div>
+                <div className='service-body'>
+                  <h5 className='service-title'>Suppport Team</h5>
+                  <p className='service-description'>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
